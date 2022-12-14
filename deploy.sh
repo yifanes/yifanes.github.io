e662a53d328760de9014c653ca16980f797c016c
yifanes <yifanes@qq.com>
2022-12-14 20:19:13 +0800

    fix deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,7 @@ set -e
 push_addr=`git remote get-url --push origin` # git提交地址，也可以手动设置，比如：push_addr=git@github.com:xugaoyi/vuepress-theme-vdoing.git
 commit_info=`git describe --all --always --long`
 dist_path=docs/.vuepress/dist # 打包生成的文件夹路径
-push_branch=master # 推送的分支
+push_branch=gh-pages # 推送的分支
 
 # 生成静态文件
 npm run build
@@ -23,3 +23,5 @@ git push -f $push_addr HEAD:$push_branch
 
 cd -
 rm -rf $dist_path
+
+git push -f $push_addr HEAD:master
